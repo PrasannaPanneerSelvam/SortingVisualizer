d759e3bf4052e9bc9a88d6e03afeb0058d676c89
Prasanna PS <52719458+PrasannaPanneerSelvam@users.noreply.github.com>
2021-11-14 23:49:00 +0530

    More sorting functions with UI for configurations

--- main.js
+++ main.js
@@ -34,6 +34,8 @@ const createBarsArray = function (range){
     };
 };
 
+
+
 const swapLogic = async function(idx1, idx2, {valueArray, divArray, delayTiming}){
 
     const sleepFn = () => new Promise(_ => setTimeout(_, delayTiming));
@@ -50,6 +52,17 @@ const swapLogic = async function(idx1, idx2, {valueArray, divArray, delayTiming}
     // Altering respective bar heights
     divArray[idx1].style.height = `${value2 * proportion}%`;
     divArray[idx2].style.height = `${value1 * proportion}%`;
+}
+
+const printAsPerCurrent = async function({valueArray, divArray, delayTiming}){
+
+    const sleepFn = () => new Promise(_ => setTimeout(_, delayTiming));
+
+    await sleepFn();
+
+    // Altering respective bar heights
+    valueArray.forEach( (itemVal, idx) => divArray[idx].style.height = `${ itemVal * proportion}%` );
+
 }
 
 // Impure function
@@ -76,6 +89,8 @@ function alterSpeed(speed){
     sortingDetails.delayTiming = delayTiming;
 }
 
+
+
 const SortingAlgos = (function (){
 
     async function bubbleSort(sortingProps){
@@ -88,20 +103,193 @@ const SortingAlgos = (function (){
                 await swapLogic(j, j+1, sortingProps);
     }
 
+    async function selectionSort(sortingProps){
+
+        const { valueArray } = sortingProps;
+
+        var i, j, min_idx, n = valueArray.length;
+    
+        for (i = 0; i < n-1; i++) {
+            min_idx = i;
+            for (j = i + 1; j < n; j++)
+                if (valueArray[j] < valueArray[min_idx])
+                    min_idx = j;
+    
+            await swapLogic(min_idx, i, sortingProps);
+        }
+    }
+
+    async function quickSort(sortingProps){
+
+        const { valueArray } = sortingProps;
+
+        async function partition (arr, low, high){ 
+            var pivot = arr[high], i = low - 1;
+        
+            for (var j = low; j <= high - 1; j++)
+                if (arr[j] < pivot)
+                    await swapLogic(++i, j, sortingProps);
+
+            await swapLogic(i+1, high, sortingProps);
+            
+            return i + 1;
+        }
+    
+        async function doQuickSort(arr, low, high){
+            if (low < high) {
+                var pivot = await partition(arr, low, high);
+                await doQuickSort(arr, low, pivot - 1); 
+                await doQuickSort(arr, pivot + 1, high); 
+            }
+        }
+
+        await doQuickSort(valueArray, 0, valueArray.length - 1);
+    }
+
+    async function heapSort(sortingProps) {
+        const { valueArray } = sortingProps;
+    
+        async function doHeapSort(arr) {
+            var n = arr.length;
+    
+            for (var i = Math.floor(n / 2) - 1; i >= 0; i--)
+                await heapify(arr, n, i);
+    
+            for (var i = n - 1; i > 0; i--) {
+                await swapLogic(0, i, sortingProps);
+                await heapify(arr, i, 0);
+            }
+        }
+    
+        async function heapify(arr, n, i) {
+
+            var largest = i, l = 2 * i + 1, r = 2 * i + 2;
+    
+            if (l < n && arr[l] > arr[largest]) largest = l;
+    
+            if (r < n && arr[r] > arr[largest]) largest = r;
+    
+            if (largest != i) {
+                await swapLogic(i, largest, sortingProps);
+                await heapify(arr, n, largest);
+            }
+        }
+
+        await doHeapSort(valueArray);
+    }
+
+
+    // Think and add a way to manipulate at the time of sorting
+    async function insertionSort(sortingProps) { 
+        
+        const { valueArray } = sortingProps;
+
+        let i, key, j, n = valueArray.length;
+
+        for (i = 1; i < n; i++) { 
+            key = valueArray[i]; 
+            j = i - 1; 
+    
+            while (j >= 0 && valueArray[j] > key){ 
+                valueArray[j + 1] = valueArray[j]; 
+                j = j - 1; 
+            }
+
+            valueArray[j + 1] = key;
+            await printAsPerCurrent(sortingProps);
+        }
+    }
+
+    // Think and add a way to manipulate at the time of sorting
+    async function mergeSort(sortingProps){
+
+        const { valueArray } = sortingProps;
+
+        async function merge(arr, l, m, r){
+            var i, j, k,
+                n1 = m - l + 1,
+                n2 = r - m;
+        
+            var L = new Array(n1), R = new Array(n2);
+        
+            for (i = 0; i < n1; i++) L[i] = arr[l + i];
+        
+            for (j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+        
+            i = 0, j = 0, k = l;
+        
+            while (i < n1 && j < n2)
+                if (L[i] <= R[j]) arr[k++] = L[i++];
+                else arr[k++] = R[j++];
+        
+            while (i < n1) arr[k++] = L[i++];
+        
+            while (j < n2) arr[k++] = R[j++];
+        }
+        
+        async function doMergeSort(arr, l, r){
+            if (l < r) {
+                var m =l+ parseInt((r-l)/2);
+
+                console.log(l, m, r);
+        
+                await doMergeSort(arr, l, m);
+                await doMergeSort(arr, m + 1, r);
+        
+                merge(arr, l, m, r);
+                await printAsPerCurrent(sortingProps);
+            }
+        }
+        
+        await doMergeSort(valueArray, 0, valueArray.length - 1);
+
+    }
+
     return {
         bubbleSort,
-    };
+        selectionSort,
+        quickSort,
+        heapSort,
 
+        insertionSort,
+        mergeSort,
+    };
 }());
 
 
 // Html elements
-const container = document.getElementById('bar-container');
+const container = document.getElementById('bar-container'),
+    buttonList = document.getElementById('button-list-list'),
+    resetArray = document.getElementById('reset-array'),
+    speedSlider = document.getElementById('speed-slider'),
+    itemCountSlider = document.getElementById('item-count-slider');
 
 let range, proportion, delayTiming, sortingDetails;
 
-// Initial setting
-alterRange(50);
-alterSpeed(5);
 
-SortingAlgos.bubbleSort(sortingDetails);
+// Initial setting
+alterRange(itemCountSlider.value * 5);
+alterSpeed(speedSlider.value);
+
+
+for(const fn of Object.values(SortingAlgos)){
+
+    // Button text from function name :: mergeSort -> Merge sort
+    const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1),
+        text = capitalize( fn.name.split('Sort')[0] ) + ' sort';
+
+    const li = document.createElement('li');
+    li.innerText = text;
+
+    li.addEventListener('click', () => fn(sortingDetails));
+
+    buttonList.append(li);
+}
+
+resetArray.addEventListener('click', () => alterRange(range));
+
+itemCountSlider.addEventListener('change', ({target}) => alterRange(target.value * 5) );
+
+speedSlider.addEventListener('change', ({target}) => alterSpeed(target.value) );
+
+// SortingAlgos.bubbleSort(sortingDetails);
